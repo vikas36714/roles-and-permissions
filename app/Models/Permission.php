002f0171a9bad0efcfd506e933c260c2058ffadf
Alok Singh <alokphpexpert@gmail.com>
2021-04-03 19:12:57 +0530

    first commit

---
+++ app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Role;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class,'roles_permissions');
+    }
+}
